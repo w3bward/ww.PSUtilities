b02d5bddb4c696bb9babe49cde2fbc42c798f869
w3bward <2651605+w3bward@users.noreply.github.com>
2022-01-24 09:16:44 -0800

    Initial Commit

---
+++ wwPSUtilities/ConvertFromDataRow.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Management.Automation;
+using System.Text;
+
+namespace wwPSUtilities
+{
+    [Cmdlet(VerbsData.ConvertFrom, "DataRow")]
+    [OutputType(typeof(PSObject))]
+    public class ConvertFromDataRow : PSCmdlet
+    {
+        [Parameter(
+            Mandatory = true,
+            Position = 0,
+            ValueFromPipeline = true)]
+        public DataRow DataRow { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            PSObject outputObject = new PSObject();
+            foreach (DataColumn column in DataRow.Table.Columns)
+            {
+                object value = DataRow.IsNull(column) ? null : DataRow[column];
+                outputObject.Properties.Add(new PSNoteProperty(column.ColumnName, value));
+            }
+
+            WriteObject(outputObject);
+        }
+    }
+}
